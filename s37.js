1c1a0abbe0cfcfc8615f388c84a2d53ff6e9549c
elizabhet <heniostroza@gmail.com>
2021-12-23 23:42:46 -0500

    s37

---
+++ s37.js
@@ -0,0 +1,54 @@
+/*const listaDeNumeros = [1, 3, 5, 7, 9, 11];
+console.log(listaDeNumeros[0])//1
+console.log(listaDeNumeros[4])//
+
+//momento de rcorrer un arrat con un bucle for
+
+let numerosImapres = [1,3,5,7];
+cantidad = numerosImapres.length;
+console.log("primera forma:")
+for(i = 0; cantidad >i; i++){
+    console.log(numerosImapres[i])
+}
+console
+
+let numerosPares = [2,4,6,8,10,12,14];
+pares= numerosPares.length;
+console.log("segunda forma:")
+for(i = 0; i < pares; i = i+2){
+    console.log(numerosPares[i])
+}
+//momento de practicar arreglos
+
+const listaDeNumeros = [13, 14, 15, 16, 17, 18];
+console.log(listaDeNumeros[2])
+listaDeNumeros[2] = 16814564;
+console.log(listaDeNumeros[2])
+
+//agregar elementos a un arreglo
+
+listadeNumeros = []
+listadeNumeros[0] = 100;
+listadeNumeros[1] = 102;
+listadeNumeros[2] = -10;
+listadeNumeros[3] = 23
+console.log(listadeNumeros[2]);*/
+
+const mixedArray = [3, "hola mundo", 3627.2];
+cantidad = mixedArray.length;
+for(i = 0; cantidad > i ; i++){
+    console.log(mixedArray[i])
+}
+
+let questions = [
+    ['¿Cuál es la capital de Perú?','Lima'],
+    ['¿Cuántos millones de peruanos somos?','35.97'],
+    ['¿Cuál es la moneda de Perú?','Soles']
+]
+
+console.log(questions[0][0]);
+console.log(questions[0][1]);
+console.log(questions[1][0]);
+console.log(questions[1][1]);
+
+
